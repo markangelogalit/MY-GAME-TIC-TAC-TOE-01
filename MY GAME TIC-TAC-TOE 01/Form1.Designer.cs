bba01d82ea2932f6a5b712a6d25accd3c0505ba3
Mark Angelo Galit <rankm12345@gmail.com>
2022-06-04 02:17:46 +0800

    deleting text name of 9 buttons

--- MY GAME TIC-TAC-TOE 01/Form1.Designer.cs
+++ MY GAME TIC-TAC-TOE 01/Form1.Designer.cs
@@ -46,7 +46,6 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(95, 76);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
@@ -56,7 +55,6 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(95, 76);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = false;
             // 
             // button3
@@ -66,7 +64,6 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(95, 76);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = false;
             // 
             // button4
@@ -76,7 +73,6 @@
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(95, 76);
             this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = false;
             // 
             // button5
@@ -86,7 +82,6 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(95, 76);
             this.button5.TabIndex = 4;
-            this.button5.Text = "button5";
             this.button5.UseVisualStyleBackColor = false;
             // 
             // button6
@@ -96,7 +91,6 @@
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(95, 76);
             this.button6.TabIndex = 5;
-            this.button6.Text = "button6";
             this.button6.UseVisualStyleBackColor = false;
             // 
             // button7
@@ -106,7 +100,6 @@
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(95, 76);
             this.button7.TabIndex = 6;
-            this.button7.Text = "button7";
             this.button7.UseVisualStyleBackColor = false;
             // 
             // button8
@@ -116,7 +109,6 @@
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(95, 76);
             this.button8.TabIndex = 7;
-            this.button8.Text = "button8";
             this.button8.UseVisualStyleBackColor = false;
             // 
             // button9
@@ -126,7 +118,6 @@
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(95, 76);
             this.button9.TabIndex = 8;
-            this.button9.Text = "button9";
             this.button9.UseVisualStyleBackColor = false;
             // 
             // Form1
