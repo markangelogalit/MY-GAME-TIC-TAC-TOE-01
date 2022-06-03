f6814915db7dc13305f249e07a1efe8fe97fc83c
Mark Angelo Galit <rankm12345@gmail.com>
2022-06-04 02:20:58 +0800

    changing name of 9 buttons

--- MY GAME TIC-TAC-TOE 01/Form1.Designer.cs
+++ MY GAME TIC-TAC-TOE 01/Form1.Designer.cs
@@ -28,97 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
+            this.buttontic1 = new System.Windows.Forms.Button();
+            this.buttontic2 = new System.Windows.Forms.Button();
+            this.buttontic3 = new System.Windows.Forms.Button();
+            this.buttontic4 = new System.Windows.Forms.Button();
+            this.buttontic5 = new System.Windows.Forms.Button();
+            this.buttontic6 = new System.Windows.Forms.Button();
+            this.buttontic7 = new System.Windows.Forms.Button();
+            this.buttontic8 = new System.Windows.Forms.Button();
+            this.buttontic9 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // buttontic1
             // 
-            this.button1.BackColor = System.Drawing.Color.Yellow;
-            this.button1.Location = new System.Drawing.Point(12, 40);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(95, 76);
-            this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = false;
+            this.buttontic1.BackColor = System.Drawing.Color.Yellow;
+            this.buttontic1.Location = new System.Drawing.Point(12, 40);
+            this.buttontic1.Name = "buttontic1";
+            this.buttontic1.Size = new System.Drawing.Size(95, 76);
+            this.buttontic1.TabIndex = 0;
+            this.buttontic1.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // buttontic2
             // 
-            this.button2.BackColor = System.Drawing.Color.Yellow;
-            this.button2.Location = new System.Drawing.Point(113, 40);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(95, 76);
-            this.button2.TabIndex = 1;
-            this.button2.UseVisualStyleBackColor = false;
+            this.buttontic2.BackColor = System.Drawing.Color.Yellow;
+            this.buttontic2.Location = new System.Drawing.Point(113, 40);
+            this.buttontic2.Name = "buttontic2";
+            this.buttontic2.Size = new System.Drawing.Size(95, 76);
+            this.buttontic2.TabIndex = 1;
+            this.buttontic2.UseVisualStyleBackColor = false;
             // 
-            // button3
+            // buttontic3
             // 
-            this.button3.BackColor = System.Drawing.Color.Yellow;
-            this.button3.Location = new System.Drawing.Point(214, 40);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(95, 76);
-            this.button3.TabIndex = 2;
-            this.button3.UseVisualStyleBackColor = false;
+            this.buttontic3.BackColor = System.Drawing.Color.Yellow;
+            this.buttontic3.Location = new System.Drawing.Point(214, 40);
+            this.buttontic3.Name = "buttontic3";
+            this.buttontic3.Size = new System.Drawing.Size(95, 76);
+            this.buttontic3.TabIndex = 2;
+            this.buttontic3.UseVisualStyleBackColor = false;
             // 
-            // button4
+            // buttontic4
             // 
-            this.button4.BackColor = System.Drawing.Color.Yellow;
-            this.button4.Location = new System.Drawing.Point(12, 122);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(95, 76);
-            this.button4.TabIndex = 3;
-            this.button4.UseVisualStyleBackColor = false;
+            this.buttontic4.BackColor = System.Drawing.Color.Yellow;
+            this.buttontic4.Location = new System.Drawing.Point(12, 122);
+            this.buttontic4.Name = "buttontic4";
+            this.buttontic4.Size = new System.Drawing.Size(95, 76);
+            this.buttontic4.TabIndex = 3;
+            this.buttontic4.UseVisualStyleBackColor = false;
             // 
-            // button5
+            // buttontic5
             // 
-            this.button5.BackColor = System.Drawing.Color.Yellow;
-            this.button5.Location = new System.Drawing.Point(113, 122);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(95, 76);
-            this.button5.TabIndex = 4;
-            this.button5.UseVisualStyleBackColor = false;
+            this.buttontic5.BackColor = System.Drawing.Color.Yellow;
+            this.buttontic5.Location = new System.Drawing.Point(113, 122);
+            this.buttontic5.Name = "buttontic5";
+            this.buttontic5.Size = new System.Drawing.Size(95, 76);
+            this.buttontic5.TabIndex = 4;
+            this.buttontic5.UseVisualStyleBackColor = false;
             // 
-            // button6
+            // buttontic6
             // 
-            this.button6.BackColor = System.Drawing.Color.Yellow;
-            this.button6.Location = new System.Drawing.Point(214, 122);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(95, 76);
-            this.button6.TabIndex = 5;
-            this.button6.UseVisualStyleBackColor = false;
+            this.buttontic6.BackColor = System.Drawing.Color.Yellow;
+            this.buttontic6.Location = new System.Drawing.Point(214, 122);
+            this.buttontic6.Name = "buttontic6";
+            this.buttontic6.Size = new System.Drawing.Size(95, 76);
+            this.buttontic6.TabIndex = 5;
+            this.buttontic6.UseVisualStyleBackColor = false;
             // 
-            // button7
+            // buttontic7
             // 
-            this.button7.BackColor = System.Drawing.Color.Yellow;
-            this.button7.Location = new System.Drawing.Point(12, 204);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(95, 76);
-            this.button7.TabIndex = 6;
-            this.button7.UseVisualStyleBackColor = false;
+            this.buttontic7.BackColor = System.Drawing.Color.Yellow;
+            this.buttontic7.Location = new System.Drawing.Point(12, 204);
+            this.buttontic7.Name = "buttontic7";
+            this.buttontic7.Size = new System.Drawing.Size(95, 76);
+            this.buttontic7.TabIndex = 6;
+            this.buttontic7.UseVisualStyleBackColor = false;
             // 
-            // button8
+            // buttontic8
             // 
-            this.button8.BackColor = System.Drawing.Color.Yellow;
-            this.button8.Location = new System.Drawing.Point(113, 204);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(95, 76);
-            this.button8.TabIndex = 7;
-            this.button8.UseVisualStyleBackColor = false;
+            this.buttontic8.BackColor = System.Drawing.Color.Yellow;
+            this.buttontic8.Location = new System.Drawing.Point(113, 204);
+            this.buttontic8.Name = "buttontic8";
+            this.buttontic8.Size = new System.Drawing.Size(95, 76);
+            this.buttontic8.TabIndex = 7;
+            this.buttontic8.UseVisualStyleBackColor = false;
             // 
-            // button9
+            // buttontic9
             // 
-            this.button9.BackColor = System.Drawing.Color.Yellow;
-            this.button9.Location = new System.Drawing.Point(214, 204);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(95, 76);
-            this.button9.TabIndex = 8;
-            this.button9.UseVisualStyleBackColor = false;
+            this.buttontic9.BackColor = System.Drawing.Color.Yellow;
+            this.buttontic9.Location = new System.Drawing.Point(214, 204);
+            this.buttontic9.Name = "buttontic9";
+            this.buttontic9.Size = new System.Drawing.Size(95, 76);
+            this.buttontic9.TabIndex = 8;
+            this.buttontic9.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
@@ -126,15 +126,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.GreenYellow;
             this.ClientSize = new System.Drawing.Size(326, 332);
-            this.Controls.Add(this.button9);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttontic9);
+            this.Controls.Add(this.buttontic8);
+            this.Controls.Add(this.buttontic7);
+            this.Controls.Add(this.buttontic6);
+            this.Controls.Add(this.buttontic5);
+            this.Controls.Add(this.buttontic4);
+            this.Controls.Add(this.buttontic3);
+            this.Controls.Add(this.buttontic2);
+            this.Controls.Add(this.buttontic1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -143,14 +143,14 @@
 
         #endregion
 
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
-        private Button button5;
-        private Button button6;
-        private Button button7;
-        private Button button8;
-        private Button button9;
+        private Button buttontic1;
+        private Button buttontic2;
+        private Button buttontic3;
+        private Button buttontic4;
+        private Button buttontic5;
+        private Button buttontic6;
+        private Button buttontic7;
+        private Button buttontic8;
+        private Button buttontic9;
     }
 }
