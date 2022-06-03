e7985a4ad5eea4cf71a29b33586677ff6411a353
Mark Angelo Galit <rankm12345@gmail.com>
2022-06-04 02:48:56 +0800

    button is working in complete code

--- MY GAME TIC-TAC-TOE 01/Form1.cs
+++ MY GAME TIC-TAC-TOE 01/Form1.cs
@@ -21,6 +21,22 @@ namespace MY_GAME_TIC_TAC_TOE_01
                 {
                     Valuebtntic = "X";
                 }
+                if (buttontic1.Text == buttontic2.Text)
+                {
+                    if (buttontic2.Text == buttontic3.Text)
+                    {
+                        MessageBox.Show("You are the Winner!" + buttontic1.Text);
+                        buttontic1.Text = "";
+                        buttontic2.Text = "";
+                        buttontic3.Text = "";
+                        buttontic4.Text = "";
+                        buttontic5.Text = "";
+                        buttontic6.Text = "";
+                        buttontic7.Text = "";
+                        buttontic8.Text = "";
+                        buttontic9.Text = "";
+                    }
+                }
             }
         }
     }
