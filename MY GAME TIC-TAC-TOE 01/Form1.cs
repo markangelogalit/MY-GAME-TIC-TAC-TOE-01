95ab25025fea4ef475531d8f064962984b1cd1a7
Mark Angelo Galit <rankm12345@gmail.com>
2022-06-04 15:30:47 +0800

    button 7 diagonal is working in complete code

--- MY GAME TIC-TAC-TOE 01/Form1.cs
+++ MY GAME TIC-TAC-TOE 01/Form1.cs
@@ -394,6 +394,22 @@ namespace MY_GAME_TIC_TAC_TOE_01
                         buttontic9.Text = "";
                     }
                 }
+                if (buttontic7.Text == buttontic5.Text)
+                {
+                    if (buttontic5.Text == buttontic3.Text)
+                    {
+                        MessageBox.Show("You are the Winner!" + buttontic7.Text);
+                        buttontic1.Text = "";
+                        buttontic2.Text = "";
+                        buttontic3.Text = "";
+                        buttontic4.Text = "";
+                        buttontic5.Text = "";
+                        buttontic6.Text = "";
+                        buttontic7.Text = "";
+                        buttontic8.Text = "";
+                        buttontic9.Text = "";
+                    }
+                }
             }
         }
 
