73c54a1ca07304eca997d1053feef0281f706b09
Mark Angelo Galit <rankm12345@gmail.com>
2022-06-04 15:35:16 +0800

    button 9 diag and ver is working in complete code

--- MY GAME TIC-TAC-TOE 01/Form1.cs
+++ MY GAME TIC-TAC-TOE 01/Form1.cs
@@ -491,6 +491,38 @@ namespace MY_GAME_TIC_TAC_TOE_01
                         buttontic9.Text = "";
                     }
                 }
+                if (buttontic9.Text == buttontic5.Text)
+                {
+                    if (buttontic5.Text == buttontic1.Text)
+                    {
+                        MessageBox.Show("You are the Winner!" + buttontic9.Text);
+                        buttontic1.Text = "";
+                        buttontic2.Text = "";
+                        buttontic3.Text = "";
+                        buttontic4.Text = "";
+                        buttontic5.Text = "";
+                        buttontic6.Text = "";
+                        buttontic7.Text = "";
+                        buttontic8.Text = "";
+                        buttontic9.Text = "";
+                    }
+                }
+                if (buttontic9.Text == buttontic6.Text)
+                {
+                    if (buttontic6.Text == buttontic3.Text)
+                    {
+                        MessageBox.Show("You are the Winner!" + buttontic9.Text);
+                        buttontic1.Text = "";
+                        buttontic2.Text = "";
+                        buttontic3.Text = "";
+                        buttontic4.Text = "";
+                        buttontic5.Text = "";
+                        buttontic6.Text = "";
+                        buttontic7.Text = "";
+                        buttontic8.Text = "";
+                        buttontic9.Text = "";
+                    }
+                }
             }
         }
     }
