641a8e332140ddd521024a46600e814c86ec640b
Mark Angelo Galit <rankm12345@gmail.com>
2022-06-04 14:35:58 +0800

    button 1 vertical is working in complete code

--- MY GAME TIC-TAC-TOE 01/Form1.cs
+++ MY GAME TIC-TAC-TOE 01/Form1.cs
@@ -36,8 +36,27 @@ namespace MY_GAME_TIC_TAC_TOE_01
                         buttontic8.Text = "";
                         buttontic9.Text = "";
                     }
+                    
+                }
+                if (buttontic1.Text == buttontic4.Text)
+                {
+                    if (buttontic4.Text == buttontic7.Text)
+                    {
+                        MessageBox.Show("You are the Winner!" + buttontic1.Text);
+                        buttontic1.Text = "";
+                        buttontic2.Text = "";
+                        buttontic3.Text = "";
+                        buttontic4.Text = "";
+                        buttontic5.Text = "";
+                        buttontic6.Text = "";
+                        buttontic7.Text = "";
+                        buttontic8.Text = "";
+                        buttontic9.Text = "";
+                    }
+
                 }
             }
+
         }
 
         private void buttontic2_Click(object sender, EventArgs e)
